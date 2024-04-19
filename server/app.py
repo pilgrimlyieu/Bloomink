0b17692f5e738d27127f227101dbc46f5da8227c
PilgrimLyieu <80107081+pilgrimlyieu@users.noreply.github.com>
2024-04-19 12:21:26 +0800

    初始化项目框架 (#3)

---
+++ server/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/api/data", methods=["GET"])
+def get_data():
+    data = {"message": "Hello, Vue.js!"}
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
