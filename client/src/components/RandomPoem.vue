598588eed10625d50ea7676652dafaf46840a716
PilgrimLyieu <PilgrimLyieu@outlook.com>
2024-05-18 19:53:22 +0800

    `RandomPoem` 组件也改用 Tab，弃用 Collapse

--- client/src/components/RandomPoem.vue
+++ client/src/components/RandomPoem.vue
@@ -1,53 +1,48 @@
 <template>
-  <div>
-    <n-space justify="center" style="margin-bottom: 3px">
-      <n-button type="primary" round @click="chooseRandomPoem"
-        >下一个</n-button
-      ></n-space
+  <n-space justify="center" style="margin-bottom: 3px">
+    <n-button type="primary" round @click="chooseRandomPoem"
+      >下一个</n-button
+    ></n-space
+  >
+  <n-card
+    :title="`📖《${getPoetryName(poemID)}》${getPoetName(poemID)}`"
+    style="width: 100%"
+    embedded
+    header-style="font-size: 18px; font-weight: bold; text-align: center"
+  >
+    {{ poetries[poemID]?.content }}
+  </n-card>
+  <n-alert name="标签" type="info" title="标签">
+    <n-tag
+      v-for="(tag, index) in poetries[poemID].tags"
+      :type="specialTagType(index)"
+      style="margin: 5px 5px 0 0; cursor: pointer; user-select: none"
+      round
+      size="large"
     >
-    <n-card
-      :title="`📖《${getPoetryName(poemID)}》${getPoetName(poemID)}`"
-      style="width: 100%"
-      embedded
-      header-style="font-size: 18px; font-weight: bold; text-align: center"
-    >
-      {{ poetries[poemID]?.content }}
-    </n-card>
-    <n-space></n-space>
-    <n-collapse>
-      <n-collapse-item name="翻译" title="翻译">
-        <span
-          v-html="t(poetries[poemID]?.fanyi.replace(/^译文\s*/, ''))"
-        ></span>
-      </n-collapse-item>
-      <n-collapse-item name="赏析" title="赏析">
-        <span v-html="t(poetries[poemID]?.shangxi)"></span>
-      </n-collapse-item>
-      <n-collapse-item name="额外信息" title="额外信息">
-        <span v-html="t(poetries[poemID]?.about)"></span>
-      </n-collapse-item>
-      <n-collapse-item name="作者" title="作者">
-        <n-image
-          :src="poetries[poemID]?.poet.image"
-          object-fit="cover"
-          size="100"
-        ></n-image>
-        <br />
-        <span v-html="t(poetries[poemID]?.poet.desc)"></span>
-      </n-collapse-item>
-      <n-collapse-item name="标签" title="标签">
-        <n-tag
-          v-for="(tag, index) in poetries[poemID].tags"
-          :type="specialTagType(index)"
-          style="margin: 5px 5px 0 0; cursor: pointer; user-select: none"
-          round
-          size="large"
-        >
-          {{ tag }}
-        </n-tag>
-      </n-collapse-item>
-    </n-collapse>
-  </div>
+      {{ tag }}
+    </n-tag>
+  </n-alert>
+  <n-tabs type="segment" animated>
+    <n-tab-pane name="翻译">
+      <span v-html="t(poetries[poemID]?.fanyi.replace(/^译文\s*/, ''))"></span>
+    </n-tab-pane>
+    <n-tab-pane name="赏析">
+      <span v-html="t(poetries[poemID]?.shangxi)"></span>
+    </n-tab-pane>
+    <n-tab-pane name="额外信息">
+      <span v-html="t(poetries[poemID]?.about)"></span>
+    </n-tab-pane>
+    <n-tab-pane name="作者">
+      <n-image
+        :src="poetries[poemID]?.poet.image"
+        object-fit="cover"
+        size="100"
+      ></n-image>
+      <br />
+      <span v-html="t(poetries[poemID]?.poet.desc)"></span>
+    </n-tab-pane>
+  </n-tabs>
 </template>
 
 <script>
